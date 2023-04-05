36d16a96fea5e639cbf3a45eca841b01d9cbe213
MaximMasyuk <40184181+MaximMasyuk@users.noreply.github.com>
2023-04-05 10:51:34 +0300

    change TransactionListAPIView to transaction_list

--- wallet_transactions/transactions/urls.py
+++ wallet_transactions/transactions/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import TransactionListAPIView, TransactionDetailAPIView, transaction_list
+from .views import (
+    transaction_list,
+    TransactionDetailAPIView,
+    transaction_list_for_name,
+)
 
 
 urlpatterns = [
-    path("", TransactionListAPIView.as_view(), name="transactions-list"),
+    path("", transaction_list, name="transactions-list"),
     path("<int:pk>/", TransactionDetailAPIView.as_view(), name="transactions-detail"),
-    path("<str:name_of_wallet>/", transaction_list),
+    path("<str:name_of_wallet>/", transaction_list_for_name),
 ]
--- wallet_transactions/transactions/views.py
+++ wallet_transactions/transactions/views.py
@@ -12,12 +12,6 @@ from .permission import IsOwnerTransaction
 # Create your views here.
 
 
-class TransactionListAPIView(generics.ListAPIView):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-    permission_classes = (IsOwnerTransaction,)
-
-
 class TransactionDetailAPIView(generics.RetrieveAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
@@ -25,12 +19,21 @@ class TransactionDetailAPIView(generics.RetrieveAPIView):
 
 
 @api_view(("GET",))
-def transaction_list(request, name_of_wallet: str):
-    print(request.user.id)
+def transaction_list(request):
+    transaction = Transaction.objects.filter(
+        Q(sender__owner=request.user.id) | Q(receiver__owner=request.user.id)
+    )
+    serializer = TransactionSerializer(transaction, many=True)
+    if not transaction:
+        return Response({"error": "transaction does not exist"})
+    return Response(serializer.data)
+
+
+@api_view(("GET",))
+def transaction_list_for_name(request, name_of_wallet: str):
     transaction = Transaction.objects.filter(
         Q(sender__owner=request.user.id) | Q(receiver__owner=request.user.id)
     ).filter(Q(sender__name=name_of_wallet) | Q(receiver__name=name_of_wallet))
-    print(request)
     serializer = TransactionSerializer(transaction, many=True)
     if not transaction:
         return Response({"error": "transaction does not exist"})
